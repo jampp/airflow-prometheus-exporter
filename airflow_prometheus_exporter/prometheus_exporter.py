b0e3fc4ce447ebb65feca4d1adfae6e5802b0a5a
Rama <ramirocharriol@gmail.com>
2020-02-07 12:36:47 -0300

    DI-326 Adding lines for readability

--- airflow_prometheus_exporter/prometheus_exporter.py
+++ airflow_prometheus_exporter/prometheus_exporter.py
@@ -494,11 +494,15 @@ REGISTRY.register(MetricsCollector())
 
 if RBAC:
     from flask_appbuilder import BaseView as FABBaseView, expose as FABexpose
+
+
     class RBACMetrics(FABBaseView):
         route_base = "/admin/metrics/"
+
         @FABexpose('/')
         def list(self):
             return Response(generate_latest(), mimetype='text')
+
     # Metrics View for Flask app builder used in airflow with rbac enabled
     RBACmetricsView = {
         "view": RBACMetrics(),
@@ -512,6 +516,7 @@ else:
         @expose("/")
         def index(self):
             return Response(generate_latest(), mimetype="text/plain")
+
     ADMIN_VIEW = [Metrics(category="Prometheus exporter", name="Metrics")]
     RBAC_VIEW = []
 
